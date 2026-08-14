f6bb63a332cfa13a29f8f58ee38eb1c06a1ea198
Wojciech Galuszewski <w.galuszewski@zkv.pl>
2026-08-14 13:30:42 +0200

    macos: wygenerowany rejestr wtyczek po dodaniu package_info_plus
    Plik generowany przez Fluttera, zmienil sie sam po dopisaniu package_info_plus
    do zaleznosci (odczyt realnej wersji aplikacji zamiast stalej w config.dart).

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_messaging
 import flutter_blue_plus_darwin
 import flutter_secure_storage_macos
 import geolocator_apple
+import package_info_plus
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
